a2c676d03972bb4d02ae0a7b5c477d93d93ad09b
Frits Daalmans <frits@consupedia.com>
2017-07-04 09:30:02 +0200

    empty records is a separate step

--- tables/ingredients/preprocessing/step.remove_percentages/remove_percentages.sh
+++ tables/ingredients/preprocessing/step.remove_percentages/remove_percentages.sh
@@ -3,13 +3,11 @@
 # TODO save percentages
 
 
-# line 2: change empty records to __DELETED__ and remove "^ 0 => "
 # line 4: remove percentages complicated case "3, 7 %"
 # line 4: remove percentages
 # line 5: if original was  " (25%)" remove the " ( )"
 # line 6: remove single quotes and trailing spaces (now 12198 entries left)
 cat |\
-  sed -e 's/^ *0 => \x27\x27,$/__DELETED__/;s/^ *0 => //g' |\
   sed -e 's/ *[0-9][0-9]* *, *[0-9][0-9]* *% */ /g' |\
   sed -e 's/ *[0-9][0-9]* *% */ /g' |\
   sed -e 's/ *( )//g' |\
