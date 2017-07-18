69280ec654fe11026fd8282991e3290f35f060e7
Frits Daalmans <frits@consupedia.com>
2017-07-18 08:34:36 +0200

    initial test, connect to MySQL works

---
+++ api-serverside/implementation/src/com/consupedia/backend/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	// from documentation https://github.com/go-sql-driver/mysql/blob/master/README.md#dsn-data-source-name:
+	// DSN (Data Source Name)
+	// The Data Source Name has a common format, like e.g. PEAR DB uses it, but without type-prefix (optional parts marked by squared brackets):
+	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
+	// A DSN in its fullest form:
+	// username:password@protocol(address)/dbname?param=value
+	// Except for the databasename, all values are optional. So the minimal DSN is: /dbname
+
+	db, err := sql.Open("mysql", "root:@tcp(10.60.218.110:3306)/consuweb?charset=utf8")
+	if err != nil {
+		panic(err)
+	}
+
+	// sql.Open() is lazy so ping
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	// stmt, err := db.Prepare("SELECT id, name FROM products WHERE id = ?")
+	stmt, err := db.Prepare("SELECT id, name FROM products")
+	if err != nil {
+		panic(err)
+	}
+
+	// id_in := 1
+
+	// rows, err := stmt.Query(id_in)
+	rows, err := stmt.Query()
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("just read record %v name %v\n", id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+
+}
