b662da089cc4e7177673f3e9404980668c94656e
Frits Daalmans <frits@consupedia.com>
2017-07-18 11:56:47 +0200

    struggled for ages with scoping rules and init() functions

--- api-serverside/implementation/src/com/consupedia/backend/webserver.go
+++ api-serverside/implementation/src/com/consupedia/backend/webserver.go
@@ -3,10 +3,23 @@ package backend
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
+var Allpathsregexp = regexp.MustCompile("^/products")
+
 func webserverhandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hi see if works %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "hi dispatcher here see if works %s %s", r.URL, r.URL.Path[1:])
+
+	// must match allpathsregexp
+	m := Allpathsregexp.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return
+	}
+	// need to dispatch depending on the right side of the URL
+
+	return
 }
 
 func Webserver() {
