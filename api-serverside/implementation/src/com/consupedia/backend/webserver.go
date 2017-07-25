69276b01c31fd7dfa157e7c10cf120076d4244b6
Frits Daalmans <frits@consupedia.com>
2017-07-25 16:08:37 +0200

    so far so good.

--- api-serverside/implementation/src/com/consupedia/backend/webserver.go
+++ api-serverside/implementation/src/com/consupedia/backend/webserver.go
@@ -8,16 +8,33 @@ import (
 	"strconv"
 )
 
-var Allpathsregexp = regexp.MustCompile("^/products")
+var Allpathsregexp = regexp.MustCompile("^/api/v1/products")
+var productidregexp = regexp.MustCompile("^[0-9][0-9]+")
 
 var Productsdb *sql.DB = nil
 
+var PRODUCTSAPI string = "/api/v1/products"
+
 var DEBUG bool = false
 
+// API "endpoint" /api/v1/products
+// provides the following x functions:
+// TODO GET /products => get list of all products with pagination and Link headers; takes additional parameters with the ? mechanism
+// GET /products/<p> => get details of 1 product
+// TODO DELETE /products/<p> => remove products table record <p> and all dependencies
+// TODO POST /products => create new product and return its id
+// TODO PUT /products/<p> => update product record
+
 func webserverproductshandler(w http.ResponseWriter, r *http.Request) {
+	var productidprovided bool = false
+	var productid int
 	if DEBUG {
 		fmt.Fprintf(w, "<html><body>\n")
 		fmt.Fprintf(w, "products dispatcher here see if works %s %s", r.URL, r.URL.Path[1:])
+	}
+
+	if DEBUG {
+		fmt.Fprintf(w, "<html><body><br/>This was a %s request, </body></html>\n", r.Method)
 	}
 
 	matchthis := r.URL.String()
@@ -25,24 +42,41 @@ func webserverproductshandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<br/>DBG matchthis &lt;%s&gt;\n", matchthis)
 	}
 
-	q := r.URL.Query()
-	if DEBUG {
-		fmt.Fprintf(w, "<br/>\nDBG q &lt;%s&gt;\n", q)
+	// first parse if productid is provided
+	// TODO
+	if matchthis[0:len(PRODUCTSAPI)] != PRODUCTSAPI {
+		fmt.Fprintf(w, "<br/>ERROR matchthis &lt;%s&gt; SHOULD NEVER GET HERE\n", matchthis)
+		return
+	} else {
+		reststring := matchthis[len(PRODUCTSAPI)+1:]
+
+		productidstring := productidregexp.FindString(reststring)
+		productidprovided = (productidstring != "")
+
+		if productidprovided {
+			if DEBUG {
+				fmt.Fprintf(w, "<br/>OK matchthis &lt;%s&gt;<br/>rest &lt;%s&gt;, productidProvided = %v\n", matchthis, reststring, productidprovided)
+				fmt.Fprintf(w, "<br/>productid to parse from &lt;%v&gt;\n", productidstring)
+			}
+			var err error
+			productid, err = strconv.Atoi(productidstring)
+			if err != nil {
+				fmt.Fprintf(w, "<br/>ERROR malformed productid &lt;%v&gt; : %v\n", productidstring, err)
+				return
+			}
+
+			if DEBUG {
+				fmt.Fprintf(w, "<br/>success, productid = %d\n", productid)
+			}
+
+		}
 	}
 
-	// q is a map like Values
-	qid := q["id"]
-	if qid != nil {
-		if DEBUG {
-			fmt.Fprintf(w, "<br/>DBG q has key id value %v\n", qid)
-		}
+	// we now have all the info to parse:
+	// r.Method and productidprovided
 
-		id64, err := strconv.ParseInt(qid[0], 10, 32)
-		if err != nil {
-			panic(err)
-		}
-		var id int = int(id64)
-		name, err := Getproductsrecord(Productsdb, id)
+	if (r.Method == "GET") && (productidprovided) { // single product query
+		name, err := Getproductsrecord(Productsdb, productid)
 		if err != nil {
 			panic(err)
 		}
@@ -52,7 +86,7 @@ func webserverproductshandler(w http.ResponseWriter, r *http.Request) {
 
 		// create JSON response
 		var responsecontainer *Container = nil
-		responsecontainer = NewJsonContainer().AddProductRecord(id, name)
+		responsecontainer = NewJsonContainer().AddProductRecord(productid, name)
 
 		jsonbytes, err := Makejson(responsecontainer.Records)
 		if err != nil {
@@ -68,7 +102,28 @@ func webserverproductshandler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
-	}
+	} // endif (GET and productidprovided)
+
+	// obsolete for now
+	// 	q := r.URL.Query()
+	// 	if DEBUG {
+	// 		fmt.Fprintf(w, "<br/>\nDBG q &lt;%s&gt;\n", q)
+	// 	}
+	//
+	// 	// q is a map like Values
+	// 	qid := q["id"]
+	// 	if qid != nil {
+	// 		if DEBUG {
+	// 			fmt.Fprintf(w, "<br/>DBG q has key id value %v\n", qid)
+	// 		}
+	//
+	// 		id64, err := strconv.ParseInt(qid[0], 10, 32)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		var productid int = int(id64)
+
+	// 	}
 
 	//	if (matchthis.len >= 9){
 	//          testslice := matchthis.URL[0:8]
@@ -93,13 +148,13 @@ func webserverresthandler(w http.ResponseWriter, r *http.Request) {
 	}
 	// need to dispatch depending on the right side of the URL
 
-	fmt.Fprintf(w, "<html><body><h1>WTF %s</hi></body></html>", r.URL)
+	fmt.Fprintf(w, "<html><body><h1>WTF %s</h1></body></html>", r.URL)
 	return
 }
 
 func Webserver(proddb *sql.DB) {
 	Productsdb = proddb
-	http.HandleFunc("/products", webserverproductshandler)
+	http.HandleFunc("/api/v1/products/", webserverproductshandler)
 	http.HandleFunc("/", webserverresthandler)
 	http.ListenAndServe(":1752", nil)
 }
