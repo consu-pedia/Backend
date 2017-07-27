8d0bb4b046ddb5469ca6ea8cb13231e81e3ed565
Frits Daalmans <frits@consupedia.com>
2017-07-27 10:37:07 +0200

    SQL time parsing fixed and made a function Sqltime2Gotime()

--- api-serverside/implementation/src/com/consupedia/backend/db.go
+++ api-serverside/implementation/src/com/consupedia/backend/db.go
@@ -27,6 +27,34 @@ func Initproductsdb() *sql.DB {
 	return (db)
 }
 
+// Conversion function from date/time values in the returned fields
+// of a sql.Rows struct, to the time.Time struct that Golang works with.
+// The format turned out to be: either nil, or "almost" ISO 8601
+// but with the T replaced by a space and no timezone info at the end.
+// TODO: return epoch when input is NULL, now it's something like year 0
+// which doesn't exist
+// It was maybe a bit silly for me to write this but I couldn't find an
+// existing implementation. Frits.
+func Sqltime2Gotime(sqltime interface{}) (gotime time.Time, err error) {
+	var tmptimebytes []byte
+	var ttime string
+
+	if sqltime != nil {
+		tmptimebytes = sqltime.([]byte)
+		//DBG		fmt.Printf("DBG Sqltime2Gotime parsing: %d %02x %02x \"%s\"\n", len(tmptimebytes), tmptimebytes[0], tmptimebytes[1], string(tmptimebytes))
+		// it's like ISO 8601 except the letter T is missing
+		ttime = strings.Replace(string(tmptimebytes), " ", "T", 1) + "Z"
+		gotime = time.Unix(0, 0)
+		err = gotime.UnmarshalText([]byte(ttime))
+		if err != nil {
+			fmt.Printf("DBG aww time conversion FAILED: %v\n", err)
+		}
+	} else {
+		gotime = time.Unix(0, 0)
+	}
+	return gotime, err
+}
+
 func ScanProduct(rows *sql.Rows) (prod Productstruct, err error) {
 	var colid int
 	var gtin string = ""
@@ -92,37 +120,11 @@ func ScanProduct(rows *sql.Rows) (prod Productstruct, err error) {
 		manufacturer_id = 0
 	}
 
-	var tmptimebytes []byte
-	var ttime string
 	// haven't the foggiest idea how to convert raw []int8 to time_t
 	// 19 bytes (too long for all time_t like formats), it's just a bloody string!
-	if createdAtHolder != nil {
-		tmptimebytes = createdAtHolder.([]byte)
-		fmt.Printf("DBG created_at parsing: %d %02x %02x \"%s\"\n", len(tmptimebytes), tmptimebytes[0], tmptimebytes[1], string(tmptimebytes))
-		// it's like ISO 8601 except the letter T is missing
-		ttime = strings.Replace(string(tmptimebytes), " ", "T", 1) + "Z"
-		created_at = time.Unix(0, 0)
-		err = created_at.UnmarshalText([]byte(ttime))
-		if err != nil {
-			fmt.Printf("DBG aww time conversion FAILED: %v\n", err)
-		}
-	} else {
-		created_at = time.Unix(0, 0)
-	}
+	created_at, err = Sqltime2Gotime(createdAtHolder)
 
-	if updatedAtHolder != nil {
-		tmptimebytes = updatedAtHolder.([]byte)
-		fmt.Printf("DBG updated_at parsing: %d %02x %02x \"%s\"\n", len(tmptimebytes), tmptimebytes[0], tmptimebytes[1], string(tmptimebytes))
-		// it's like ISO 8601 except the letter T is missing
-		ttime = strings.Replace(string(tmptimebytes), " ", "T", 1) + "Z"
-		updated_at = time.Unix(0, 0)
-		err = updated_at.UnmarshalText([]byte(ttime))
-		if err != nil {
-			fmt.Printf("DBG aww time conversion FAILED: %v\n", err)
-		}
-	} else {
-		updated_at = time.Unix(0, 0)
-	}
+	updated_at, err = Sqltime2Gotime(updatedAtHolder)
 
 	fmt.Printf("ScanProduct(): just read record %v name %v\n", colid, name)
 	prod = Productstruct{Type: TYPE_PRODUCT, Id: colid, Gtin: gtin, Name: name, Fullname: fullname, Size: size, SizeunitId: sizeunitId, Image: image, Bulk: bulk, Description: description, CategoryId: category_id, BrandId: brand_id, ManufacturerId: manufacturer_id, CreatedAt: &created_at, UpdatedAt: &updated_at}
