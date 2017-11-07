e5b9f1a2ea023f2a6561d359d03f2f387d17e08d
Frits Daalmans <frits@consupedia.com>
2017-11-07 13:42:29 +0100

    fixed a bug with multiples of 1000 e.g. linolsyra 28 000

--- tables/ingredients/willys/preprocessing/step.mark_quantities/step.mark_quantities.sh
+++ tables/ingredients/willys/preprocessing/step.mark_quantities/step.mark_quantities.sh
@@ -6,17 +6,19 @@
 # line 3 marks 1/4 msk etc.
 # line 4 marks ½ gul lök etc.
 # line 5 marks 1 ½ morot etc.
-# line 6 marks rest of the numbers
-# line 7 parses formal (SI) units directly after a number
-# line 8 parses informal units
-# line 9 parses formal (SI) units after a number and a space
-# line 10 TODO parses "56 mg/100 ml", "8 g / 100 g" etc.
-# line 11: if there are no units, put a space back
+# line 6 marks 28 000 etc.
+# line 7 marks rest of the numbers
+# line 8 parses formal (SI) units directly after a number
+# line 9 parses informal units
+# line 10 parses formal (SI) units after a number and a space
+# line 11 TODO parses "56 mg/100 ml", "8 g / 100 g" etc.
+# line 12: if there are no units, put a space back
 cat |\
   sed -e 's?\(^\|[^a-z]\)\([1-9][0-9]*\)  *\([1-9]/[1-9]\) ?QUANT_\2_\3_QUANT?g;' |\
   sed -e 's?\(^\|[^a-z]\)\([1-9]/[1-9]\) ?QUANT_\2_QUANT?g;' |\
   sed -e 's?\(^\|[^a-z0-9]\)\(¼\|½\|¾\)? QUANT_\2_QUANT?g;' |\
   sed -e 's?\(^\|[^a-z]\)\([1-9][0-9]*\) *\(¼\|½\|¾\)? QUANT_\2\3_QUANT?g;' |\
+  sed -e 's?\(^\|[^a-z0-9]\)\([0-9][0-9]*\) \([0-9][0-9]*\)\([^0-9]\|$\)? QUANT_\2\3_QUANT\4?g;' |\
   sed -e 's?\(^\|[^a-z0-9]\)\([0-9][0-9]*\) *, *\([0-9][0-9]*\)\([^0-9]\|$\)? QUANT_\2,\3_QUANT\4?g;' |\
   sed -e 's?\(^\|[^a-z]\)\([1-9][0-9]*\) ? QUANT_\2_QUANT?g;' |\
   sed -e 's?\(^\|[^a-z]\)\([1-9][0-9]*\) *\(g\|mg\|kg\|ml\|dl\)\( \|[^0-9a-z]\)? QUANT_\2_QUANT UNIT_\3_UNIT\4?g;' |\
