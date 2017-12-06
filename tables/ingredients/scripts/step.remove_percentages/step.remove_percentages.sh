24569185f60bb167300d557d2d8caeac295bb6b5
Frits Daalmans <frits@consupedia.com>
2017-12-06 11:34:16 +0100

    step.remove_percentages.sh

---
+++ tables/ingredients/scripts/step.remove_percentages/step.remove_percentages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# TODO save percentages
+
+
+# line 4: remove percentages complicated case "3, 7 %"
+# line 4: remove percentages
+# line 5: if original was  " (25%)" remove the " ( )"
+# line 6: remove single quotes and trailing spaces (now 12198 entries left)
+cat |\
+  sed -e 's/QUANT_[^_]*_QUANT %//g' |\
+  sed -e 's/ *[0-9][0-9]* *, *[0-9][0-9]* *% */ /g' |\
+  sed -e 's/ *[0-9][0-9]* *% */ /g' |\
+  sed -e 's/ *( )//g' |\
+  sed -e 's/^ *\x27//;s/ *//;s/ *\x27$//;s/^ *//;' |\
+  cat
+
+exit 0
+
