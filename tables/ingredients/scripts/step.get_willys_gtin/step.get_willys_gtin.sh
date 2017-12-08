526a50bb5cc82543982ec54f672712849f210549
Frits Daalmans <frits@consupedia.com>
2017-12-08 15:28:02 +0100

    updated willys initial phase upto normalization. Discovered a new bug: make the primary key of the gtin_table.sql file begin at the last one of the previous shop. Now I have duplicate primary keys for oldcoop and willys, which is not good.

--- tables/ingredients/scripts/step.get_willys_gtin/step.get_willys_gtin.sh
+++ tables/ingredients/scripts/step.get_willys_gtin/step.get_willys_gtin.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+SHOP="willys"
 
 cat > $OUTDIR/tmp.step1a.mainstream
 
 cat $OUTDIR/tmp.step1a.mainstream |\
   grep '"ean" *: *"' |\
-  awk -F'"' '{OFS="\"";printf("%d", NR); printf(""); printf("%s",$4);for(w=5;w<=NF-1;w++){printf("\"%s",$(w));};printf("\n");}' |\
-  cat > $OUTDIR/tmp1a.product_gtin_table.raw
+#TOO COMPLICATED  awk -F'"' -v"shop=$SHOP" '{OFS="\"";printf("%d", NR); printf(""); printf("%s",$4);for(w=5;w<=NF-1;w++){printf("\"%s",$(w));};printf("\"%s\n", shop);}' |\
+  awk -F'"' -v"shop=$SHOP" '{printf("%d%s%s\n", NR, $4, shop);}' |\
+  cat > $OUTDIR/gtin_table.raw
+
+# convert gtin table to SQL
+cat $OUTDIR/gtin_table.raw |\
+  awk -F'' '{ printf("INSERT INTO gtintable VALUES ( %d, \"%s\", \"%s\" );\n", $1, $3, $2);}' |\
+  cat > $OUTDIR/gtin_table.sql
 
 cat $OUTDIR/tmp.step1a.mainstream
 rm $OUTDIR/tmp.step1a.mainstream
